6228c0262f642ee82a7ad85767b5cd13994cb468
niemtec <niemtec@users.noreply.github.com>
2021-03-13 12:56:52 +0000

    Added mocks. Completed lesson 7

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -13,11 +13,18 @@ class LocationDetail extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: _renderFacts(location),
+          children: _renderBody(context, location),
         ));
   }
 
-  List<Widget> _renderFacts(Location location) {
+  List<Widget> _renderBody(BuildContext context, Location location) {
+    List<Widget> result = [];
+    result.add(_bannerImage(location.url, 170.00));
+    result.addAll(_renderFacts(context, location));
+    return result;
+  }
+
+  List<Widget> _renderFacts(BuildContext context, Location location) {
     List<Widget> result = [];
     for (int i = 0; i < location.facts!.length; i++) {
       result.add(_sectionTitle(location.facts![i].title));
@@ -27,10 +34,24 @@ class LocationDetail extends StatelessWidget {
   }
 
   Widget _sectionTitle(String text) {
-    return Text(text);
+    return Container(
+        padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
+        child: Text(
+          text,
+          textAlign: TextAlign.left,
+          style: TextStyle(fontSize: 25.0, color: Colors.black),
+        ));
   }
 
   Widget _sectionText(String text) {
-    return Text(text);
+    return Container(
+        padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
+        child: Text(text));
+  }
+
+  Widget _bannerImage(String url, double height) {
+    return Container(
+        constraints: BoxConstraints.tightFor(height: height),
+        child: Image.network(url, fit: BoxFit.fitWidth));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'location_detail.dart';
 import 'mocks/mock_location.dart';
-
 import 'models/location.dart';
+import 'location_list.dart';
 
 void main() {
-  final Location mockLocation = MockLocation.FetchAny();
+  final mockLocations = MockLocation.fetchAll();
 
-  return runApp(MaterialApp(home: LocationDetail(mockLocation)));
+  return runApp(MaterialApp(home: LocationList(mockLocations)));
 }
